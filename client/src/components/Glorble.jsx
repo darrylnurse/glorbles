7fe53a757562fb3edb5b91ec9b940d53b26e37bc
darcierre <darrnurse@gmail.com>
2024-10-18 23:02:57 -0400

    Glorble design update

--- client/src/components/Glorble.jsx
+++ client/src/components/Glorble.jsx
@@ -12,32 +12,47 @@ export default function Glorble ({ id, color, hat, shoes, glasses, price, create
 
   return (
       <div
-          className={'glorble-body cursor-pointer relative z-0 flex justify-center items-center'}
+          className={'glorble-body cursor-pointer relative z-[0] flex justify-center items-center'}
           style={{backgroundColor: color, opacity: "inherit"}}
           onClick={handleNav}
       >
+        <div
+            className={'glorble-body glorble-outline cursor-pointer absolute z-[0]'}
+            style={{backgroundColor: color}}
+            onClick={handleNav}
+        ></div>
         <img
             src={String(hat).includes('none') ? null : hat}
             alt={''}
-            className={'absolute top-[-30%] z-2'}
+            className={'absolute top-[-30%] z-[2]'}
         />
         <img
             src={String(glasses).includes('none') ? null : glasses}
             alt={''}
-            className={'absolute top-[15%] z-1'}
+            className={'absolute top-[15%] z-[4]'}
         />
         <img
             src={'/src/assets/glorble-face.png'}
             alt={'glorble-face'}
-            className={'w-[80%]'}
+            className={'w-[80%] absolute z-[1]'}
         />
         <div
-            className={'glorble-feet absolute right-[-10%] bottom-0'}
+            className={'glorble-feet glorble-feet-outline z-[2] absolute right-[-10%] bottom-0'}
             style={{backgroundColor: color}}
         >
         </div>
         <div
-            className={'glorble-feet absolute left-[-10%] bottom-0'}
+            className={'glorble-feet glorble-feet-outline z-[2] absolute left-[-10%] bottom-0'}
+            style={{backgroundColor: color}}
+        >
+        </div>
+        <div
+            className={'glorble-feet z-[1] absolute right-[-10%] bottom-0'}
+            style={{backgroundColor: color}}
+        >
+        </div>
+        <div
+            className={'glorble-feet z-[1] absolute left-[-10%] bottom-0'}
             style={{backgroundColor: color}}
         >
         </div>
